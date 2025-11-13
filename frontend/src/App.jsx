ec189fadce4b3fb9539992d5b837dae767cd7c4c
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-11-13 12:37:58 +0200

    Add HeroSection component to App.jsx for Transportation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HeroSection from './components/transport/HeroSection';
 
 function App() {
   return (
-    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-700">
-        🚀 Tailwind is working!
-        Hurrayyyyyyy
-      </h1>
-    </div>
-  )
+      <>
+        <HeroSection />
+      </>
+  );
 }
 
-export default App
+export default App;
