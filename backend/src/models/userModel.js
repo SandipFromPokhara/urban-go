1dca40099738d221c92cf555074e0757707e022c
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-11-17 00:06:50 +0200

    Modify userModel, added new property dob and preferences.

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,24 +1,48 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-    {
-        name : {
-            type: String, 
-            required: true,
-        },
-        email: {
-            type: String, 
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true
-        }
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    preferences: [
+      {
+        type: String,
+        required: false,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
