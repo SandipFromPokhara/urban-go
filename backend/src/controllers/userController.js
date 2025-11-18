45128b487bad5b4927fd9cf69710454b6c0397d6
SaileshK1103 <sailesh.karki@metropolia.fi>
2025-11-18 15:01:43 +0200

    feat: setup MongoDB connection and initial user feature files.

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
+
+// GET /users
+const getAllUsers = async (req,res) => {
+    
+}
+
+// POST /users
+const createUser = async (req, res) = {
+
+}
+
+// GET /users/:userId
+const getUserById = async (req,res) => {
+
+}
+
+// PATCH /users/:userId
+const updateUser = async (req, res) => {
+
+}
+
+// DELETE /user/:userId
+const deleteUser = async (req, res) => {
+    
+}
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+const connectDB = require("./config/db")
+
+connectDB();
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
