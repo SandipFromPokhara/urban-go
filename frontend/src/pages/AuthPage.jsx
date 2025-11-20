64e125a2c6252f416887a053a32a05555ed0e34a
Twe He Gam Aung <Gam.TweHe@metropolia.fi>
2025-11-20 21:07:58 +0200

    Fixed padding

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,3 +1,4 @@
+// AuthPage.jsx
 import React, { useState } from "react";
 import Login from "../components/signup/Login.jsx";
 import Signup from "../components/signup/Signup.jsx";
@@ -6,38 +7,48 @@ const AuthPage = () => {
   const [page, setPage] = useState("login");
 
   return (
-    <div>
-      {/* Render Login or Signup form based on state */}
-      {page === "login" ? <Login /> : <Signup />}
-
-      {/* Navigation text below the form */}
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: "20px",
-        fontSize: "16px",
-      }}>
-        {page === "login" ? (
-          <span>
-            Don't have an account?{" "}
-            <span
-              style={{ color: "#2563eb", cursor: "pointer", textDecoration: "underline" }}
-              onClick={() => setPage("signup")}
-            >
-              Press here to register
+    <div className="auth-outer-wrapper">
+      <div className="auth-inner-content">
+        {page === "login" ? <Login /> : <Signup />}
+        {/* Switch links */}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+            fontSize: "16px",
+          }}
+        >
+          {page === "login" ? (
+            <span>
+              Don't have an account?{" "}
+              <span
+                style={{
+                  color: "#2563eb",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                onClick={() => setPage("signup")}
+              >
+                Press here to register
+              </span>
             </span>
-          </span>
-        ) : (
-          <span>
-            Already have an account?{" "}
-            <span
-              style={{ color: "#2563eb", cursor: "pointer", textDecoration: "underline" }}
-              onClick={() => setPage("login")}
-            >
-              Press here to login
+          ) : (
+            <span>
+              Already have an account?{" "}
+              <span
+                style={{
+                  color: "#2563eb",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                onClick={() => setPage("login")}
+              >
+                Press here to login
+              </span>
             </span>
-          </span>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
