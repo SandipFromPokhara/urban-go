1e26673d6af43b3363fbe392b62347e7acbd665f
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-11-27 19:53:00 +0200

    Remove unnecessary react import

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,59 @@
+// AuthPage.jsx
+import { useState } from "react";
+import Login from "../components/signup/Login.jsx";
+import Signup from "../components/signup/Signup.jsx";
+
+const AuthPage = () => {
+  const [page, setPage] = useState("login");
+
+  return (
+    <section className="hero-section">
+      <div className="auth-outer-wrapper">
+        <div className="auth-inner-content scroll-wrapper">
+          {page === "login" ? <Login /> : <Signup />}
+          {/* Switch links */}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "20px",
+              fontSize: "16px",
+            }}
+          >
+            {page === "login" ? (
+              <span>
+                Don't have an account?{" "}
+                <span
+                  style={{
+                    color: "#2563eb",
+                    cursor: "pointer",
+                    textDecoration: "underline",
+                  }}
+                  onClick={() => setPage("signup")}
+                >
+                  Press here to register
+                </span>
+              </span>
+            ) : (
+              <span>
+                Already have an account?{" "}
+                <span
+                  style={{
+                    color: "#2563eb",
+                    cursor: "pointer",
+                    textDecoration: "underline",
+                  }}
+                  onClick={() => setPage("login")}
+                >
+                  Press here to login
+                </span>
+              </span>
+            )}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AuthPage;
