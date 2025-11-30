12fd837c55d8ec6caf43db25ca225f322e30253f
Dinal Maha Vidanelage <dinalm@metropolia.fi>
2025-11-30 14:49:02 +0200

    Code updated

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-const PORT = process.env.TEST_PORT || 5000;
+const PORT = process.env.TEST_PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Test server listening on port ${PORT}`);
 });
