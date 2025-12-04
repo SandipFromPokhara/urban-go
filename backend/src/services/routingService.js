c71ec598cdb8abfbdf1097b612e35c2669e27ed2
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-12-04 22:51:55 +0200

    Update transport mode names for route display

--- backend/src/services/routingService.js
+++ backend/src/services/routingService.js
@@ -1,36 +1,78 @@
 // backend/src/services/routingService.js
 
-const getRoutes = async (fromCoords, toCoords, dateTime ) => {
+const getRoutes = async (fromCoords, toCoords, dateTime) => {
   let dateArg = "";
   if (dateTime) {
     const dt = new Date(dateTime);
     const dateStr = dt.toISOString().split("T")[0]; // yyyy-mm-dd
     const timeStr = dt.toTimeString().split(" ")[0]; // HH:mm:ss
-    dateArg = `date: "${dateStr}", time: "${timeStr}"`;
+    dateArg = `date: "${dateStr}", time: "${timeStr}", arriveBy: false`;
   }
 
   const query = `{
-            plan(
-                from: {lat: ${fromCoords.lat}, lon: ${fromCoords.lon}}
-                to: {lat: ${toCoords.lat}, lon: ${toCoords.lon}}
-                numItineraries: 5
-                ${dateArg}
-            ) {
-                itineraries {
-                    duration
-                    walkDistance
-                    legs {
-                        mode
-                        startTime
-                        endTime
-                        distance
-                        from { name lat lon }
-                        to { name lat lon }
-                        route { shortName longName }
-                    }
-                }
+    plan(
+      from: { lat: ${fromCoords.lat}, lon: ${fromCoords.lon} }
+      to: { lat: ${toCoords.lat}, lon: ${toCoords.lon} }
+      numItineraries: 5
+      ${dateArg}
+    ) {
+      itineraries {
+        duration
+        walkDistance
+        legs {
+          mode
+          distance
+          duration
+          startTime
+          endTime
+          realTime
+
+          from {
+            name
+            lat
+            lon
+            stop {
+              code
+              name
+              platformCode
+              zoneId
+              lat
+              lon
             }
-        }`;
+          }
+          to {
+            name
+            lat
+            lon
+            stop {
+              code
+              name
+              platformCode
+              zoneId
+              lat
+              lon
+            }
+          }
+
+          route {
+            shortName
+            longName
+          }
+
+          intermediatePlaces {
+            name
+            stop {
+              name
+              code
+              lat
+              lon
+              platformCode
+            }
+          }
+        }
+      }
+    }
+  }`;
 
   const transportUrl = process.env.TRANSPORT_URL;
   const transportUrlResponse = await fetch(transportUrl, {
@@ -43,7 +85,6 @@ const getRoutes = async (fromCoords, toCoords, dateTime ) => {
   });
 
   const json = await transportUrlResponse.json();
-
   return json?.data?.plan?.itineraries || [];
 };
 
