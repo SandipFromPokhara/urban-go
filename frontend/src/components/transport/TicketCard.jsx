a02af7617999bc474a29b20cacd970ac87b5cff6
Sandip Ranjit <sandip.ranjit@metropolia.fi>
2025-12-10 00:58:09 +0200

    Set ticket prices according to HSL rates

--- frontend/src/components/transport/TicketCard.jsx
+++ frontend/src/components/transport/TicketCard.jsx
@@ -3,10 +3,10 @@
 import { FaTicketAlt, FaExternalLinkAlt } from "react-icons/fa";
 
 const tickets = [
-  { zone: "A", price: "€2.90" },
-  { zone: "AB", price: "€4.10" },
-  { zone: "ABC", price: "€5.00" },
-  { zone: "Day ticket", price: "€9.00" },
+  { zone: "AB", price: "3.20 €" },
+  { zone: "ABC", price: "4.40 €" },
+  { zone: "ABCD", price: "4.80 €" },
+  { zone: "Day ticket", price: "10.00 €" },
 ];
 
 export default function TicketCard({ isDarkMode }) {
@@ -20,7 +20,7 @@ export default function TicketCard({ isDarkMode }) {
       {/* Header with glow */}
       <div className={`flex items-center gap-2 mb-4 p-3 rounded-xl shadow-md ${headerGradient} text-white`}>
         <div className="p-2 bg-white/20 rounded-full backdrop-blur-sm shadow-lg animate-pulse">
-          <FaTicketAlt size={24} />
+          <FaTicketAlt size={40} />
         </div>
         <h3 className="font-bold text-lg">Ticket Info based on zones</h3>
       </div>
@@ -29,9 +29,9 @@ export default function TicketCard({ isDarkMode }) {
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {tickets.map((ticket) => {
           const color = {
-            A: "from-green-200 to-green-300 text-green-800",
-            AB: "from-blue-200 to-blue-300 text-blue-800",
-            ABC: "from-purple-200 to-purple-300 text-purple-800",
+            AB: "from-green-200 to-green-300 text-green-800",
+            ABC: "from-blue-200 to-blue-300 text-blue-800",
+            ABCD: "from-purple-200 to-purple-300 text-purple-800",
             "Day ticket": "from-yellow-200 to-yellow-300 text-yellow-800",
           }[ticket.zone];
 
